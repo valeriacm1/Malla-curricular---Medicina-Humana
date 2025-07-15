80273794ac00630deab6091f5d6a61245bc536cb
valeriacm1 <vmunozh14@icloud.com>
2025-07-14 21:07:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function marcarCurso(curso) {
+  // Si el curso está habilitado, lo apruebas
+  if (curso.classList.contains('habilitado')) {
+    curso.classList.remove('habilitado');
+    curso.classList.add('aprobado');
+    curso.innerText = curso.innerText + " - Aprobado";
+  } 
+  // Si el curso está deshabilitado, lo habilitas
+  else if (curso.classList.contains('deshabilitado')) {
+    curso.classList.remove('deshabilitado');
+    curso.classList.add('habilitado');
+    curso.innerText = curso.innerText.replace(" - No disponible", "") + " - Habilitado";
+  }
+  // Si el curso ya está aprobado, cambia a deshabilitado
+  else if (curso.classList.contains('aprobado')) {
+    curso.classList.remove('aprobado');
+    curso.classList.add('deshabilitado');
+    curso.innerText = curso.innerText.replace(" - Aprobado", "") + " - No disponible";
+  }
+}
 
+// Inicializar los cursos como habilitados o deshabilitados
+document.querySelectorAll('.curso').forEach(curso => {
+  if (curso.classList.contains('habilitado')) {
+    curso.innerText = curso.innerText + " - Habilitado";
+  } else if (curso.classList.contains('deshabilitado')) {
+    curso.innerText = curso.innerText + " - No disponible";
+  }
+});
